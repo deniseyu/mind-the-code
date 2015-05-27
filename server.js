1547a56a57d0157c0228ae3661abc9b6c323bd9b
Denise <yu.denise.d@gmail.com>
2015-05-27 20:27:43 +0100

    node server done

---
+++ server.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
